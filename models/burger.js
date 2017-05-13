a643fe2303fbd3971ec81a55aecefa9291ef51a9
cookies4u <jcreyes.1018@gmail.com>
2017-05-13 00:53:49 -0700

    production connection added in config file

--- models/burger.js
+++ models/burger.js
@@ -4,9 +4,14 @@ module.exports = function(sequelize, DataTypes) {
     burger_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [1, 80]
+      }
     },
     devoured: {
       type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
     },
     date: {
       type: DataTypes.DATE
